3d79031d240aa6343c2e2b5de18239056d3e2070
zahin <zahin.dev@gmail.com>
2019-02-23 17:29:19 -0500

    Switched to react-csv

--- src/containers/FileDownload.jsx
+++ src/containers/FileDownload.jsx
@@ -5,60 +5,42 @@ import IconButton from '@material-ui/core/IconButton';
 import { searchStyle } from './theme';
 import { connect } from 'react-redux';
 import Download from '@material-ui/icons/CloudDownload';
-
+import { CSVLink } from 'react-csv';
+import { prettifyCourseName } from '../utils/courseName';
 class FileDownload extends React.Component {
   constructor(props) {
     super(props);
     this.classes = props.classes;
+    this.state = {
+      csvData: []
+    };
   }
 
   exportCsv = () => {
-    var csvRow = [];
-    var csvData = [['', '#', 'Course Code', 'Course Tittle', 'Link']];
-    var inputCourses = this.props.input;
-    var plannerCourses = this.props.courseData;
-    var tempCourse;
-    var countNum = 1;
+    var csvData = [];
+
+    const { plannerCourses, inputCourses } = this.props;
 
     if (plannerCourses) {
-      csvData.push(['The courses you have taken:']);
+      csvData.push([
+        'The courses you can take ',
+        'Course Name',
+        'Title',
+        'Link'
+      ]);
 
-      inputCourses.forEach(inputCourses => {
-        tempCourse = inputCourses.split(/(\d+)/);
-        csvData.push([
-          countNum++,
-          tempCourse[0] + ' ' + tempCourse[1] + tempCourse[2]
-        ]);
+      plannerCourses.forEach(course => {
+        csvData.push(['', `${course[0]}`, course[1], course[2]]);
+      });
+      csvData.push(['The courses you have taken', 'Course Name']);
+
+      inputCourses.forEach(course => {
+        csvData.push(['', prettifyCourseName(course)]);
       });
 
-      csvData.push(['The courses you can take:']);
-
-      countNum = 1;
-
-      plannerCourses.forEach(plannerCourses => {
-        csvData.push([
-          countNum++,
-          plannerCourses[0],
-          plannerCourses[1].replace(/,/g, ''),
-          plannerCourses[2]
-        ]);
+      this.setState({
+        csvData
       });
-
-      csvData.forEach(csvData => {
-        csvRow.push(csvData.join(','));
-      });
-
-      var csvString = csvRow.join('\n');
-
-      var download = document.createElement('a');
-      download.href = 'data:text/csv;charset=utf-8' + csvString;
-      download.target = '_Blank';
-      download.download = 'EZPlanner.csv';
-      document.body.appendChild(download);
-      download.click();
-    } else {
-      // TODO: display error message
-      console.warn('No course input!!!'); // eslint-disable-line no-console
     }
   };
 
@@ -68,24 +50,32 @@ class FileDownload extends React.Component {
         color="primary"
         className={this.classes.iconButton}
         aria-label="Enter"
-        onClick={this.exportCsv}
+        onClick={() => this.exportCsv()}
         component="label"
       >
-        <Download />
+        <CSVLink
+          data={this.state.csvData}
+          separator={','}
+          filename={'ezplanner.csv'}
+          className="btn btn-primary"
+          target="_blank"
+        >
+          <Download />
+        </CSVLink>
       </IconButton>
     );
   }
 }
 
 FileDownload.propTypes = {
-  courseData: PropTypes.array,
-  input: PropTypes.array,
+  plannerCourses: PropTypes.array,
+  inputCourses: PropTypes.array,
   classes: PropTypes.object
 };
 
 const mapStateToProps = state => ({
-  courseData: state.plannerCourses,
-  input: state.coursesInput
+  plannerCourses: state.plannerCourses,
+  inputCourses: state.coursesInput
 });
 
 export default withStyles(searchStyle)(
--- src/utils/courseName.js
+++ src/utils/courseName.js
@@ -2,3 +2,8 @@ export const prettifyCourseName = course => {
   const regexObj = /(^[^\d]+)(.*)/.exec(course);
   return `${regexObj[1]} ${regexObj[2]}`;
 };
+
+export const prettifyCourseNameArray = course => {
+  const regexObj = /(^[^\d]+)(.*)/.exec(course);
+  return [`${regexObj[1]}`, `${regexObj[2]}`];
+};
